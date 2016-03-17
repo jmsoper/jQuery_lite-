bd7a334707b05fd86a810a055f4aa7f273afadd8
JuliaAndDan <JuliaAndDan@appacademy.io>
2016-03-17 11:37:08 -0400

    Create DOM node collection

--- jquery_lite.js
+++ jquery_lite.js
@@ -1,3 +1,17 @@
 (function(root) {
-  console.log("YUP");
+  "use strict";
+
+  var DOMNodeCollection = function(htmlEls){
+    this.htmlEls = htmlEls;
+  };
+
+  root.$l = function ( arg ) {
+    if (typeof arg === "string"){
+      var htmlEls = document.querySelectorAll(arg);
+      return new DOMNodeCollection(htmlEls);
+    }
+
+
+  };
+
 })(this);
