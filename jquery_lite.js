ac89610da2f804a4319a08018507fb1aac0d611a
JuliaAndDan <JuliaAndDan@appacademy.io>
2016-03-17 12:17:24 -0400

    Implement DOMNodeCollection#append

--- jquery_lite.js
+++ jquery_lite.js
@@ -3,21 +3,43 @@
 
   var DOMNodeCollection = function(htmlEls){
     this.htmlEls = htmlEls;
+    this.length = htmlEls.length;
   };
 
   DOMNodeCollection.prototype.html = function (string) {
     if (typeof string === "undefined") {
       return this.htmlEls[0].innerHTML;
     } else {
-      for(var i = 0; i < this.htmlEls.length; i++) {
+      for(var i = 0; i < this.length; i++) {
         this.htmlEls[i].innerHTML = string;
       }
     }
   };
 
+  // DOMNodeCollection.prototype.length = function () {
+  //   return this.htmlEls.length;
+  // };
+
   DOMNodeCollection.prototype.empty = function () {
-    for(var i = 0; i < this.htmlEls.length; i++) {
+    for(var i = 0; i < this.length; i++) {
       this.htmlEls[i].innerHTML = "";
+    }
+  };
+
+  DOMNodeCollection.prototype.append = function (input) {
+    var injectContent = "";
+    if (input instanceof DOMNodeCollection){
+      for (var i = 0; i < input.length; i++) {
+        injectContent += input.htmlEls[i].outerHTML;
+      }
+    } else if ( input instanceof HTMLElement ) {
+      injectContent += input.outerHTML;
+    } else {
+      injectContent += input;
+    }
+    for (var i = 0; i < this.length; i++ ) {
+      var existingHTML = this.htmlEls[i].innerHTML;
+      this.htmlEls[i].innerHTML = existingHTML + injectContent;
     }
   };
 
