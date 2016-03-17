0eccd1fa3d73b48dfbc6a79a8d39dc354d180f1b
JuliaAndDan <JuliaAndDan@appacademy.io>
2016-03-17 11:51:37 -0400

    Implement DOMNodeCollection#html

--- jquery_lite.js
+++ jquery_lite.js
@@ -5,10 +5,22 @@
     this.htmlEls = htmlEls;
   };
 
+  DOMNodeCollection.prototype.html = function (string) {
+    if (typeof string === "undefined") {
+      return this.htmlEls[0].innerHTML;
+    } else {
+      for(var i = 0; i < this.htmlEls.length; i++) {
+        this.htmlEls[i].innerHTML = string;
+      }
+    }
+  };
+
   root.$l = function ( arg ) {
     if (typeof arg === "string"){
       var htmlEls = document.querySelectorAll(arg);
       return new DOMNodeCollection(htmlEls);
+    } else if (arg instanceof HTMLElement) {
+      return new DOMNodeCollection([arg]);
     }
 
 
